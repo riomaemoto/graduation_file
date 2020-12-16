fe240f3547a98819f8b3e53c24d08bab924ac1fd
Leo A <>
2020-12-17 04:05:13 +0900

    ハンバーガーメニュー

--- japanese school 卒業課題/js/app.js
+++ japanese school 卒業課題/js/app.js
@@ -1,21 +1,11 @@
 $(function() {
-  // ハンバーガーメニューが押された時
-  $('#toggle').on('click',function(){
-      $(this).toggleClass('on');
-      $('.js-drower').toggleClass('on');
-      
-      let isActive = $(this).hasClass('on');
-      toggleDrower(isActive);
+  $('.hamburger').click(function() {
+      $(this).toggleClass('active');
+
+      if ($(this).hasClass('active')) {
+          $('.globalMenuSp').addClass('active');
+      } else {
+          $('.globalMenuSp').removeClass('active');
+      }
   });
 });
-
-//ハンバーガーメニューでドロップダウン
-  function toggleDrower(isActive) {
-    if (isActive) {
-      // onになっていた時、メニューを表示
-      $('#drower-bg').fadeIn(600);
-    } else {
-      // onを外した時、メニューを非表示
-      $('#drower-bg').fadeOut(600);
-    }
-  }
